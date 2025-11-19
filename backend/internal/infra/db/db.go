ed5ad260517d969a421a851156d835b3cc762f73
j-ordep <joaoppinheiro2005@gmail.com>
2025-11-19 01:01:19 -0300

    feat: add db - infra

---
+++ backend/internal/infra/db/db.go
@@ -0,0 +1,46 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	"github.com/j-ordep/mjcp/backend/config"
+	_ "github.com/lib/pq"
+)
+
+var DB *sql.DB
+
+func NewConnect() (*sql.DB, error) {
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		config.Config.DBHost,
+		config.Config.DBPort,
+		config.Config.DBUser,
+		config.Config.DBPassword,
+		config.Config.DBName,
+	)
+
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open database: %w", err)
+	}
+
+	db.SetMaxOpenConns(25)
+	db.SetMaxIdleConns(5)
+	db.SetConnMaxLifetime(5 * time.Minute)
+
+	if err = db.Ping(); err != nil {
+		return nil, fmt.Errorf("failed to ping database: %w", err)
+	}
+
+	DB = db
+	return db, nil
+}
+
+func Close() error {
+	if DB != nil {
+		return DB.Close()
+	}
+	return nil
+}
