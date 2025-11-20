6e54220b578968ac4033730dc06ef81b7019382d
j-ordep <joaoppinheiro2005@gmail.com>
2025-11-20 17:57:47 -0300

    feat: login dto

--- backend/internal/dto/user.go
+++ backend/internal/dto/user.go
@@ -6,16 +6,21 @@ import (
 	"github.com/j-ordep/mjcp/backend/internal/domain/entity"
 )
 
+type LoginUserInput struct {
+	Email    string `json:"email"`
+	Password string `json:"password" binding:"required"`
+}
+
+type LoginUserOutput struct {
+	User  UserOutput `json:"user"`
+	Token string     `json:"token"`
+}
+
 type CreateUserInput struct {
 	Name     string `json:"name"`
 	Email    string `json:"email"`
 	Password string `json:"password" binding:"required,min=6"`
 	Phone    string `json:"phone"`
-}
-
-type LoginUserInput struct {
-	Email    string `json:"email"`
-	Password string `json:"password" binding:"required"`
 }
 
 type UserOutput struct {
@@ -38,10 +43,10 @@ func ToUserDomain(input CreateUserInput) (*entity.User, error) {
 
 func FromUserDomain(user *entity.User) UserOutput {
 	return UserOutput{
-		ID: user.ID,
-		Name: user.Name,
-		Email: user.Email,
-		Phone: user.Phone,
+		ID:        user.ID,
+		Name:      user.Name,
+		Email:     user.Email,
+		Phone:     user.Phone,
 		CreatedAt: user.CreatedAt,
 		UpdatedAt: user.UpdatedAt,
 	}
