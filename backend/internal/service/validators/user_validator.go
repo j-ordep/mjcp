f31857706d0cbd60a24ab3c67a9983f5ff0a02b7
j-ordep <joaoppinheiro2005@gmail.com>
2025-11-22 18:04:52 -0300

    feat: add user validator

---
+++ backend/internal/service/validators/user_validator.go
@@ -0,0 +1,37 @@
+package validators
+
+import (
+    "github.com/j-ordep/mjcp/backend/internal/domain/entity"
+    "github.com/j-ordep/mjcp/backend/internal/domain/errors"
+    "github.com/j-ordep/mjcp/backend/internal/domain/repository"
+)
+
+type UserValidator struct {
+    repo repository.UserRepository
+}
+
+func NewUserValidator(repo repository.UserRepository) *UserValidator {
+    return &UserValidator{repo: repo}
+}
+
+func (v *UserValidator) ValidateUser(user *entity.User) error {
+    existingUser, err := v.repo.GetByEmail(user.Email)
+    
+    if err != nil && err != errors.ErrUserNotFound {
+        return err
+    }
+    
+    if existingUser == nil {
+        return nil
+    }
+    
+    if existingUser.Email == user.Email {
+        return errors.ErrDuplicatedEmail
+    }
+    
+    if existingUser.Phone == user.Phone {
+        return errors.ErrDuplicatedPhone
+    }
+    
+    return nil
+}
