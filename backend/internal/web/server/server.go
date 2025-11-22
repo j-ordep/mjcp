3e2271cc1e4408d6093e40bea75c0cb5f8f9d0c8
j-ordep <joaoppinheiro2005@gmail.com>
2025-11-22 11:51:12 -0300

    feat: add routes create user and login

--- backend/internal/web/server/server.go
+++ backend/internal/web/server/server.go
@@ -6,18 +6,22 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/j-ordep/mjcp/backend/internal/service"
+	"github.com/j-ordep/mjcp/backend/internal/web/handler"
 )
 
 type Server struct {
 	router *chi.Mux
 	server *http.Server
 	port   string
+	userService *service.UserService
 }
 
-func NewServer(port string) *Server {
+func NewServer(port string, userService *service.UserService) *Server {
 	return &Server{
 		router: chi.NewRouter(),
 		port:   port,
+		userService: userService,
 	}
 }
 
@@ -27,15 +31,14 @@ func (s *Server) ConfigureRoutes() {
 	s.router.Use(middleware.Recoverer)
 	s.router.Use(middleware.SetHeader("Content-Type", "application/json"))
 
-	// Handlers
-	// userHandler := handler.NewUserHandler()
-	// scheduleHandler := handler.NewScheduleHandler()
 
-	// Rotas
-	// s.router.Get("/health", healthCheck)
-	// s.router.Post("/user", userHandler.Create)
-	// s.router.Get("/user", userHandler.GetAll)
-	// s.router.Post("/schedules", scheduleHandler.Create)
+	userHandler := handler.NewUserHandler(s.userService)
+
+	s.router.Get("/health", healthCheck)
+	s.router.Post("/user", userHandler.Create)
+	s.router.Get("/user", userHandler.GetAll)
+	s.router.Post("/login", userHandler.Login)
+
 }
 
 func healthCheck(w http.ResponseWriter, r *http.Request) {
