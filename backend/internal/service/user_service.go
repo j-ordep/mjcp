305bb04c0c655cf5847856c51b223b90261ce1d1
j-ordep <joaoppinheiro2005@gmail.com>
2025-11-22 11:50:34 -0300

    feat: login service

--- backend/internal/service/user_service.go
+++ backend/internal/service/user_service.go
@@ -4,6 +4,8 @@ import (
 	"github.com/j-ordep/mjcp/backend/internal/domain/errors"
 	"github.com/j-ordep/mjcp/backend/internal/domain/repository"
 	"github.com/j-ordep/mjcp/backend/internal/dto"
+	"github.com/j-ordep/mjcp/backend/internal/infra/jwt"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserService struct {
@@ -14,22 +16,36 @@ func NewUserService(repo repository.UserRepository) *UserService {
 	return &UserService{repo: repo}
 }
 
+func (s *UserService) Login(input dto.LoginUserInput) (*dto.LoginUserOutput, error) {
+	user, err := s.repo.GetByEmail(input.Email)
+	if err != nil {
+		return nil, err
+	}
+
+	if bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(input.Password)) != nil {
+		return nil, errors.ErrInvalidCredentials
+	}
+
+	token, err := jwt.GenerateToken(user.ID, user.Name, user.Email)
+	if err != nil{
+		return nil, errors.ErrInvalidCredentials
+	}
+
+	userOutput := dto.FromUserDomain(user)
+
+    return &dto.LoginUserOutput{
+        User:  userOutput,
+        Token: token,
+    }, nil
+}
+
 func (s *UserService) Create(input dto.CreateUserInput) (*dto.UserOutput, error) {
 	user, err := dto.ToUserDomain(input)
 	if err != nil {
 		return nil, err
 	}
 
-    // verificar se user já existe
-
-	// 	GetByEmail() → retorna (nil, ErrUserNotFound)
-	// ├─ err != nil? SIM
-	// ├─ err != ErrUserNotFound? NÃO ← Ignora o erro
-	// ├─ existingUser != nil? NÃO
-	// └─ Cria o usuário
     existingUser, err := s.repo.GetByEmail(user.Email)
-	// tem erro? se tiver, e o erro for diferenten de ErrUserNotFound, ignoramos, como isso temos existingUser
-	// se tem erro e o erro e é difefente de ErrUserNotFound, paramos
     if err != nil && err != errors.ErrUserNotFound {
         return nil, err
     }
