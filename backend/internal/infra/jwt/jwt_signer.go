2030add35c50ac23bd81bc528cc756c3d02af687
j-ordep <joaoppinheiro2005@gmail.com>
2025-11-22 11:46:23 -0300

    feat: generate token jwt

---
+++ backend/internal/infra/jwt/jwt_signer.go
@@ -0,0 +1,33 @@
+package jwt
+
+import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+	"github.com/j-ordep/mjcp/backend/config"
+)
+
+func GenerateToken(userID, userName, userEmail string) (string, error) {
+	secret := config.Config.JwtSecret
+	if secret == "" {
+		return "", errors.New("missing JWT_SECRET")
+	}
+
+	claims := jwt.MapClaims{
+		"sub":   userID,
+		"name":  userName,
+		"email": userEmail,
+		"iat":   time.Now().Unix(),
+		"exp":   time.Now().Add(time.Hour * 24).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenString, err := token.SignedString([]byte(secret))
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
